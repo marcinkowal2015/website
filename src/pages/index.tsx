67675191c39f2ea1d7d96741eee89d166974d505
Marcin Kowal <marcink@medius-group.com>
2019-12-28 22:36:11 +0100

    Added emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import {Layout} from "../components/layout";
 
 const IndexPage = () => (
-  <>
+  <Layout>
     Under construction
-  </>
+  </Layout>
 )
 
 export default IndexPage;
